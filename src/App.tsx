707127dc252beb9006d382d184efa846408ffc2a
jinu <wlsdn5153@naver.com>
2024-06-25 21:59:32 +0900

    touchmultiplier 수정

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
         lerp: 0.05,
         wheelMultiplier: 0.7,
         syncTouch: true,
-        touchMultiplier: 0.5,
+        touchMultiplier: 0.3,
         touchInertiaMultiplier: 30,
       }}
     >
