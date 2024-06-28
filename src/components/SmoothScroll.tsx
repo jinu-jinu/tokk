3f1ffdf9db904a043345cbb442495ef374d3cc49
jinu <wlsdn5153@naver.com>
2024-06-28 10:31:40 +0900

    터치옵션 수정

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -7,7 +7,7 @@ const SmoothScroll = ({ children }: { children: ReactNode }) => {
     syncTouch: true,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     wheelMultiplier: 0.5,
-    duration: 1.8,
+    duration: 1.2,
     touchMultiplier: 1,
   });
 
